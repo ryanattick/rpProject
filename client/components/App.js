26f430557cb435432912ce665049d8f6a24e55bf
Ryan <your_email@whatever.comclear>
2017-11-10 13:12:34 -0800

    server/flickr setup

--- client/components/App.js
+++ client/components/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      photos: []
     };
   }
+
+componentWillMount () {
+  $.get('/test', (data) => {
+    this.setState({
+      photos: [data]
+    })
+  })
+}
+
 
 
   render() {
     return (
       <div className="App">
-        <h1> WORKING? </h1>
+        {/* {this.state.photos[0]} */}
       </div>
     );
   }
---
+++ server/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+var flickrAuth = require('../flickr.config.js');
+var Flickr = require("node-flickr");
+var keys = {"api_key": flickrAuth.api_key}
+flickr = new Flickr(keys);
+
+app.use(express.static(__dirname + '/../builds'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get('/test', function (req, res) {
+  flickr.get("photos.search", {"tags":"cat,dogs"}, function(err, result){
+    if (err) return console.error(err);
+    console.log(result.photos.photo);
+    res.send(result.photos.photo);
+  });
+})
+
+
+
+
+
+app.listen(3000, function() {
+  console.log('listening on port 3000!');
+});
