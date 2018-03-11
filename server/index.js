11e35528fbaa66a9002aa7363b20a334621f8b29
Spencer Attick <spencerattick@Spencers-MacBook-Air.local>
2018-03-10 20:59:11 -0800

    build update

--- server/index.js
+++ server/index.js
@@ -12,9 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
+var port = process.env.PORT || 3000;
 
-
-
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+app.listen(port, function() {
+  console.log(`listening on port ${port}!`);
 });
